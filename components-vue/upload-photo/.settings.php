0128af40ac050b7db72613e86eaf5c18ad68ea07
Денис Богданов <denx-b@mail.ru>
2021-02-23 10:18:54 +0200

    Компоненты в корень проекта

--- components-vue/upload-photo/.settings.php
+++ components-vue/upload-photo/.settings.php
@@ -1,4 +1,4 @@
-<?
+<?php
 return [
     'require' => [
         'https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js'
--- local/php_interface/s4/init.php
+++ local/php_interface/s4/init.php
@@ -1,3 +1,5 @@
 <?
 require_once $_SERVER['DOCUMENT_ROOT'] .'/bitrix/vendor/autoload.php';
-define('DBOGDANOFF_VUE_MINIFY', true);
+
+define('DBOGDANOFF_VUE_MINIFY', 'hard');
+define('DBOGDANOFF_VUE_PATH', '/components-vue');
